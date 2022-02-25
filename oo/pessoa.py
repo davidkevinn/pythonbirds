bd513f156b62206d12e28725c292bec11bc1a4fa
unknown <davidkevin987@outlook.com>
2022-02-25 16:15:22 -0300

    Criados os atributos de isnstancia idade e noome

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,17 @@
 class Pessoa:
-    pass 
+    def __init__(self, nome=None, idade=35):
+        self.nome = nome
+        self.idade = idade
+        
+
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+
+if __name__ == '__main__':
+    p = Pessoa('David')
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+    print(p.idade)
+    print(p.nome)
